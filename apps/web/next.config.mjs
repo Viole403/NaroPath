5aee7dd7190f4d33b284ec0ff6e858a1dc56101a
Masalief Maulana <m.maulana.fb26@gmail.com>
2025-04-12 01:55:26 +0700

    feat: :wrench: Update Next.js configuration to include remote image patterns and set API_URL environment variable

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
+  images: {
+    remotePatterns: [
+      {
+        hostname: "**",
+      },
+    ],
+  },
+  env: {
+    API_URL: process.env.API_URL || 'http://localhost:3001',
+  },
 }
 
 export default nextConfig
